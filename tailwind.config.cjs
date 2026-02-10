e8a79ee9fa8221a207871e21b378c58ad3506220
tithyghosh <tithy.ghosh.07@gmail.com>
2026-02-11 03:08:18 +0600

    Completed the nav section and banner slider section with daisy ui

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        "bike-primary": "#e76f51",
+        "bike-primary-bg": 'rgba(231, 111, 81, 0.1)',
+      },
+    },
+  },
+};
